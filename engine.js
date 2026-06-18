ade0e6ea5eba9209fb79b54076c6eea03f59c112
Pieti30tv <juli.clement@icloud.com>
2026-06-18 21:22:56 +0200

    refactor: rewrite app with new approach

--- engine.js
+++ engine.js
@@ -1,14 +1,56 @@
-import { ChessBoard, ChessPiece } from "./lists.js";
+let squares = {};
+let activeSquare = null;
+const startPos = {
+  1: "♜",
+  2: "♞",
+  3: "♝",
+  4: "♛",
+  5: "♚",
+  6: "♝",
+  7: "♞",
+  8: "♜",
+  9: "♟",
+  10: "♟",
+  11: "♟",
+  12: "♟",
+  13: "♟",
+  14: "♟",
+  15: "♟",
+  16: "♟",
+  49: "♙",
+  50: "♙",
+  51: "♙",
+  52: "♙",
+  53: "♙",
+  54: "♙",
+  55: "♙",
+  56: "♙",
+  57: "♖",
+  58: "♘",
+  59: "♗",
+  60: "♕",
+  61: "♔",
+  62: "♗",
+  63: "♘",
+  64: "♖",
+};
 
-let board = new ChessBoard();
-let piece = new ChessPiece();
+for (let i = 1; i <= 64; i++) {
+  squares[i] = document.getElementById(String(i));
+  if (startPos[i]) {
+    squares[i].textContent = startPos[i];
+  }
 
-board.initClickListener();
+  squares[i].addEventListener("click", () => {
+    if (squares[i].textContent) {
+      activeSquare = squares[i];
+      console.log(`square ${i} clicked`);
+      console.log(activeSquare);
+      if (activeSquare !== null) {
+        activeSquare = null;
+      }
+    }
+  });
+}
 
-console.log(board);
-console.log(piece);
-console.table(piece.startPieces);
-
-// board.innerHTML = piece.startPieces[0].symbol;
-
-board.displayPieces(piece.startPieces);
+console.log(squares);
